634d0d08ee66c2c715fc9d5419930733bb023564
rootrsk <ravishankar7050@gmail.com>
2020-08-21 14:19:32 +0530

    init

---
+++ copied.js
@@ -0,0 +1,39 @@
+base: "stations"
+clouds:
+    all: 75
+    cod: 200
+coord:
+    lat: 24.75
+    lon: 85
+    dt: 1597982945
+    id: 1271439
+main:
+    feels_like: 304.6
+    humidity: 83
+    pressure: 1003
+    temp: 301.15
+    temp_max: 301.15
+    temp_min: 301.15
+
+name: "Gaya"
+sys:
+    country: "IN"
+    id: 9115
+    sunrise: 1597967818
+    sunset: 1598014178
+    type: 1
+    timezone: 19800
+    visibility: 5000
+weather: 
+    0;
+        description: "mist"
+        icon: "50d"
+        id: 701
+        main: "Mist"
+
+wind:
+    deg: 70
+    speed: 4.1  
+
+
+
--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,5 @@
 import React, { useState ,useEffect} from 'react'
 import Axios from 'axios'
-const forecast = require('../forecast')
 
 const GeoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
 const ApiKey = 'access_token=pk.eyJ1Ijoicm9vdHJzayIsImEiOiJjazVnOWRqeDAwNTFxM2RwYmptcWJmYnBpIn0.VeWzTx2FJA6zIbjDDmfbYQ&limit=1'
@@ -8,54 +7,118 @@ const ApiKey = 'access_token=pk.eyJ1Ijoicm9vdHJzayIsImEiOiJjazVnOWRqeDAwNTFxM2Rw
 function App() {
 
     const [address, setAddress] = useState('')
+    const [weather,setWeather] = useState('')
+    const [buttonStatus, setButtonStatus] = useState('&#128512;')
+    
     const adressInputHandler = (e) =>{
         setAddress(e.target.value)
 
     }
     const searchButtonHandler = async (e) =>{
+        if(!address){
+            setButtonStatus('Enter Location')
+            return
+        }
+        setButtonStatus('Loading...')
         const url = GeoCodeUrl + encodeURIComponent(address) + '.json?'+ApiKey
         const response = await Axios({
             url:url,
             method: 'get',
             
         })
+        const APIKEY = `a2344865c9890e3fd12f284af2ec6a65`
         const place_name = response.data.features[0].place_name
         const latitude = response.data.features[0].center[1]
         const longitude = response.data.features[0].center[0]
-        console.log(response)
-        const url2 = 'https://api.darksky.net/forecast/97efc916259805f85a7ad8a5d10c6561/'+latitude+','+longitude+'?units=auto'
-        const r = await forecast(latitude, longitude, place_name, (error, data) => {
-            console.log(data)
-            console.log(error)
+        const OpenWeatherMapUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKEY}`
+        const weather_response = await Axios({
+            url:OpenWeatherMapUrl
         })
-        console.log(r)
-        console.log(url2);
-        
+
+        const weather = {
+            place_name,
+            weather : weather_response.data
+        }
+        setWeather(weather)
+        setButtonStatus('&#128526;')
+    
+        return
     }
     
     
     
     useEffect(() => {
+        document.getElementById('load').innerHTML = buttonStatus
         
-    }, []);
+    }, [buttonStatus]);
     return (
-        <div>
-            <h1>Weather App</h1>
-            <div>
+        <div className='main'>
+            <h1>Weather</h1>
+            <div className='search'>
                 <input 
                     onChange={adressInputHandler}
                     defaultValue={address}
+                    placeholder='Enter Location'
+                    onKeyPress={(e)=>{
+                        if (address) setButtonStatus('&#128521;')
+                        if(e.key==='Enter') searchButtonHandler()
+                    }}
                 />
+                <div className="button_prop">
+                    <p id='load'> {buttonStatus} </p>
+                    <button 
+                        onClick={searchButtonHandler}
+                        className='button'>
+                        Search
+                    </button>
+
+
+                </div>
+                
             </div>
-            <button onClick={searchButtonHandler}>
-                Search
-            </button>
-            <div>
-                <h2>Details </h2>
-                {console.log(address)}
+            
+            <div className='weather_div'> 
+                {weather?<RenderTemp weather={weather} /> :null}
             </div>
         </div>
     )
+}
+
+
+const RenderTemp = (props) =>{
+
+    const icon = props.weather.weather.weather[0].icon
+    return(
+        <div className="weathers_details">
+
+            <div className="location_details">
+                <p>{props.weather.place_name}</p>
+
+            </div>
+            <div className="main_details">
+                <div className='temp'>
+                    <h1 id='temp'>{Math.floor(props.weather.weather.main.temp-273)}&#8451;</h1>
+                </div>
+                <div className='icon'>
+                    <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt='icon' />
+
+                </div>
+
+            </div>  
+            <div className="other_details">
+                <p>{props.weather.weather.weather[0].description.toUpperCase()}</p>
+
+            </div>
+            
+
+            
+            
+
+
+            {console.log(props.weather)}
+        </div>
+    )
+
 }
 
 export default App
