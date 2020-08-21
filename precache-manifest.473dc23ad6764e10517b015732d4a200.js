3b7b3fec7e24d14d2b9990b218a6295c5b3594e3
rootrsk <ravishankar7050@gmail.com>
2020-08-21 14:57:04 +0530

    Updates

---
+++ precache-manifest.473dc23ad6764e10517b015732d4a200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2595ae5f3b628c1768cfcdee1d0b91",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "30da009b989f83e65040",
+    "url": "/weather/static/css/main.143682fe.chunk.css"
+  },
+  {
+    "revision": "b406960115b5403b748b",
+    "url": "/weather/static/js/2.fb52e523.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.fb52e523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30da009b989f83e65040",
+    "url": "/weather/static/js/main.f9d21834.chunk.js"
+  },
+  {
+    "revision": "1557ce1959b3418744e2",
+    "url": "/weather/static/js/runtime-main.80a89714.js"
+  }
+]);
